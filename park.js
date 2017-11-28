7cf9c59f091af2f16ca14db795b270496e0e84a5
Alex Smith <alex_smith3@hotmail.com>
2017-11-28 20:48:11 +0000

    passing all tests

--- park.js
+++ park.js
@@ -3,10 +3,18 @@ var Park = function(){
   this.add = function(dinosaur){
     this.enclosure.push(dinosaur);
   };
-  this.remove = function(type){
-    for (var i=0, iLen=this.enclosure.length; i<iLen; i++) {
-      if (this.enclosure[i].type == type){
-        this.enclosure.splice(i, 1);}  
+  this.removeType = function(type){
+    for (var i=0; i<this.enclosure.length; i++) {
+      if (this.enclosure[i].type === type){
+        this.enclosure.splice(i, 1);
+      }
+    }
+  };
+  this.removeMoreThanTwoOffspring = function(){
+    for (var i=0; i<this.enclosure.length; i++) {
+      if (this.enclosure[i].offspring > 2){
+        this.enclosure.splice(i, 1);
+      }
     }
   };
 
--- specs/park_specs.js
+++ specs/park_specs.js
@@ -7,19 +7,29 @@ describe('park tests', function(){
     park = new Park();
     dino1 = new Dinosaur("velociraptor", 6);
     dino2 = new Dinosaur("triceratops", 1);
+    dino3 = new Dinosaur("velociraptor", 6);
+    dino4 = new Dinosaur("Apatosaurus", 1);
   })
   it("should start with empty enclosures", function(){
-    assert.deepStrictEqual(park.enclosure, [])
+    assert.deepStrictEqual(park.enclosure, []);
   });
   it("should be able to add a dinosaur to an enclosure", function(){
     park.add(dino1);
-    assert.strictEqual(park.enclosure.length, 1)
+    assert.strictEqual(park.enclosure.length, 1);
   });
   it("should be able to remove all dinosaurs of a certain type", function(){
     park.add(dino1);
     park.add(dino2);
-    park.remove("velociraptor")
-    assert.deepStrictEqual(park.enclosure, [dino2])
+    park.add(dino3);
+    park.add(dino4);
+    park.removeType("velociraptor");
+    assert.deepStrictEqual(park.enclosure, [dino2, dino4]);
   });
-  it("should be able to get all dinosaurs with more than 2 offspring")
+  it("should be able to get all dinosaurs with more than 2 offspring", function(){
+    park.add(dino1);
+    park.add(dino2);
+    park.removeMoreThanTwoOffspring();
+    assert.deepStrictEqual(park.enclosure, [dino2]);
+  });
+
 })
